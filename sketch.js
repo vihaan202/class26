97c56d95eeb7a0f3b7bc8f74cb964f40b751d1d1
vihaan202 <xantersover@gmail.com>
2020-11-03 15:52:11 +0530

    log6

--- sketch.js
+++ sketch.js
@@ -33,6 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(200,100,100,PI/2);
 
     bird = new Bird(100,100);
 
@@ -60,5 +61,7 @@ function draw(){
     log5.display();
 
     bird.display();
+
     platform.display();
+    log6.display();
 }
